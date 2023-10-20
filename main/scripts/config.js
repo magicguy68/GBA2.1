6e3190f650a59e0e70f98f64a104a900675793cf
magicguy68 <stonecaden907@gmail.com>
2023-10-20 13:48:57 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ main/scripts/config.js
@@ -0,0 +1,13 @@
+/* 
+    This file is used for user configuration/overrides.
+    As of now, it's only used for the romLocation variable.
+*/
+
+let relativePath = 'https://rawcdn.githack.com/Math-Study/gba-host/1a4c17810e582ddf73675cd234bc979c28756a0c/';
+let n64Location = 'https://rawcdn.githack.com/Math-Study/n64-host/868f4271286e5f8813605b2a873005dc881d642a/';
+
+let ndsLocation = 'https://rawcdn.githack.com/mathadventure1/nds-host/d4fab0b759e31d196f364333ecc0ab8842cc1203/'
+
+localStorage.setItem('romLocation', relativePath);
+localStorage.setItem('ndsLocation', ndsLocation);
+localStorage.setItem('n64Location', n64Location);
